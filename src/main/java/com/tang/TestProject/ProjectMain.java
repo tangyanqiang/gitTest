a4a90c207758c7feceff219da69fc41d9760ba6b
tangyanqiang <925288055@qq.com>
2019-04-13 18:09:26 +0800

    修改main方法

--- src/main/java/com/tang/TestProject/ProjectMain.java
+++ src/main/java/com/tang/TestProject/ProjectMain.java
@@ -4,5 +4,6 @@ public class ProjectMain {
 
 	 public static void main(String[] args) {
 		System.out.println("this is git test Project111");
+		System.out.println("this is git test Project111");
 	}
 }
