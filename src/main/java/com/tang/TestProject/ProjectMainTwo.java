a55f3f9c9aae7dd5d62adf0c854a13b6c0ad51f3
tangyanqiang <925288055@qq.com>
2019-04-13 18:10:58 +0800

    增加projectMainTwo类

---
+++ src/main/java/com/tang/TestProject/ProjectMainTwo.java
@@ -0,0 +1,8 @@
+package com.tang.TestProject;
+
+public class ProjectMainTwo {
+	public static void main(String[] args) {
+		System.out.println("this is git test Project111");
+		System.out.println("this is git test Project222");
+	}
+}
